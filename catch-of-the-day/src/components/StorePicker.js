b02f1087c5f8b913c9ed1ae2410894b11c77ef76
Tristano Costa <tristano-costa@g-star.com>
2018-04-07 04:27:36 +0200

    Importing non default export from helpers file, use defaultValue

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
   render() {
     return (
       <form className="store-selector">
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store name" />
+      <input type="text" required placeholder="Store name" defaultValue={getFunName()} />
         <button type="submit">Visit Store</button>
       </form>
     );
