a8839bc85fa762ffedb314ead88a7c3138a45ed8
Tristano Costa <tristano-costa@g-star.com>
2018-04-08 23:13:44 +0200

    Added fish form, add and update state, propagate a method with props

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,22 @@ import Order from "./Order"
 import Inventory from "./Inventory"
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    // 1. Take a copy of existing state
+    const fishes = {...this.state.fishes};
+    //2. Add new fish
+    fishes[`fish${Date.now()}`] = fish;
+    //3. Set the new fishes object to state
+    this.setState({
+      fishes : fishes // because key and value are the same it can be used only fishes once
+    })
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +27,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seefood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
